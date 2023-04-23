2e63f1ae28e62958a9ded11c79364506a83a0f4c
Marina <mrnkslv@gmail.com>
2023-04-24 01:27:35 +0300

    implementation

--- signature/signature.cpp
+++ signature/signature.cpp
@@ -7,31 +7,60 @@
 #include <functional>
 #include <experimental/filesystem>
 #include <cmath>
+#include <algorithm>
 
 const int BLOCK_SIZE = 1024 * 1024; // размер блока в байтах
 
+void fillBuffer(char* src, size_t length, size_t max_data_length, std::string& buffer) {
+    buffer = "";
+    for (int i = 0; i < max_data_length; i++) {
+        buffer += src[i];
+    }
+    int paddingCount = length - max_data_length;
+    for (int i = 0; i < paddingCount; i++) {
+        buffer += char(0);
+    }
+}
+
 int main(int argc, char* argv[]) {
     
-    std::string file_name = "hello.txt";
-    std::fstream file;
-    file.open(file_name, std::ios_base::in | std::ios_base::binary);
+    std::string input_filepath = "hello.txt";
+    std::fstream input_file;
+    input_file.open(input_filepath, std::ios_base::in | std::ios_base::binary);
+
+    std::string output_filepath = "hello_out.txt";
+    std::fstream output_file;
+    output_file.open(output_filepath, std::ios_base::out | std::ios_base::binary);
     
-    if (!file.is_open()) {
+    if (!input_file.is_open() && !output_file.is_open()) {
 
         return 1;
     }
 
-    int file_size = std::experimental::filesystem::file_size(file_name) + 1;
+    int file_size = std::experimental::filesystem::file_size(input_filepath) + 1;
     char* bytes = new char[file_size];
-    file.read(bytes, file_size);
-    delete[]bytes;
+    input_file.read(bytes, file_size);
 
     std::cout << bytes << std::endl;
 
     size_t batches = ceil((double)file_size / (double)BLOCK_SIZE);
 
-    for ()
+    std::string buffer;
+    buffer.reserve(BLOCK_SIZE);
+    std::hash<std::string> hasher;
 
+    for (int i = 0; i < batches; i++) {
+        fillBuffer((bytes + i * BLOCK_SIZE), BLOCK_SIZE, file_size, buffer);
+
+        auto hashValue = hasher(buffer);
+
+        output_file << hashValue;
+    }
+
+    input_file.close();
+    output_file.close();
+    delete[]bytes;
+    
     return 0;
 }
 
